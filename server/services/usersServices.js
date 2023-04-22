afff8756a057c5acd80251bfc95c40848ef50def
shourovahmd <moh.abuhanifa@gmail.com>
2023-04-23 03:11:31 +0600

    aggregate working

--- server/services/usersServices.js
+++ server/services/usersServices.js
@@ -1,14 +1,22 @@
 const User = require("../models/userModel");
 
 async function usersServices({ car, income, gender, phone_price }) {
-  const users = await User.where("car")
-    .equals(car)
-    .where("income")
-    .lt(`$${income}`)
-    .where("gender")
-    .equals(gender)
-    .where("phone_price")
-    .gt(phone_price);
+  const users = await User.aggregate([
+    {
+      $match: { car: car },
+    },
+    {
+      $match: { income: { $gt: `$${income}` } },
+    },
+  ]);
+  //.where("car")
+  // .equals(car)
+  // .where("income")
+  // .lt(`$${income}`)
+  // .where("gender")
+  // .equals(gender)
+  // .where("phone_price")
+  // .gt(phone_price);
   return users;
 }
 
