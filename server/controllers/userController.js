16ddc5a60561dcf386ba320582e9cd49eaabe18a
shourovahmd <moh.abuhanifa@gmail.com>
2023-04-23 02:04:18 +0600

    try catch

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,9 +1,16 @@
 const usersServices = require("../services/usersServices");
 
 async function usersController(req, res) {
-  const { id } = req.query;
-  const users = await usersServices();
-  res.send({ query: id, data: users });
+  try {
+    const { id } = req.query;
+    const users = await usersServices();
+    res.send({ query: id, data: users });
+  } catch (error) {
+    res.status(400).json({
+      status: "failed",
+      error: "Couldn't create the brand",
+    });
+  }
 }
 
 module.exports = usersController;
