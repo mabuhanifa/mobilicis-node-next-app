c6c1ec7c0083c53ae5aff59de891ffa9c9203ac8
shourovahmd <moh.abuhanifa@gmail.com>
2023-04-23 00:58:01 +0600

    db connected

--- server/server.js
+++ server/server.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const cors = require("cors");
+const db = require("./db");
 const dotenv = require("dotenv");
 const app = express();
 const port = process.env.PORT || 5000;
-
+const uri = process.env.MONGO_URI;
 app.use(express.json());
 app.use(cors());
 dotenv.config();
+db();
 
-app.get("/", (req, res) => {
+app.get("/users", async (req, res) => {
   res.send({
+    data: JSON.stringify(),
     message: "Hello World!",
   });
 });
